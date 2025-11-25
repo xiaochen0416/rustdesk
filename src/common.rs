d2c59747d40fdadda67c00996e50f3d60dbde878
xiaochen0416 <woshimcxiaochen@gmail.com>
2025-11-25 22:25:19 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1046,7 +1046,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://xiaochen.cyou:21114".to_owned()
 }
 
 #[inline]
